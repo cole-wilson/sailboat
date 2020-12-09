2e401cde022541a6604d044138f34d6a256d5a8e
Cole Wilson <cole@colewilson.xyz>
2020-12-09 18:15:31 +0000

    Release v0.0.2dev3

--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@ except:
 
 setuptools.setup(
     name="sailboat",
-    version="0.0.2dev2",
+    version="0.0.2dev3",
 		scripts=['bin/sailboat'],
 #		entry_points={
 #			'console_scripts': ['sailboat=sailboat.__main__'],
