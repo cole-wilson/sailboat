324073856cb5891236689cf26b3223773b246374
Cole Wilson <cole@colewilson.xyz>
2020-12-11 21:20:17 +0000

    Big Version!

--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@ except:
 
 options = {
 	"name":"sailboat",
-	"version":"0.0.6",
+	"version":"0.0.6.1",
 	"scripts":[],
 	"entry_points":{
 		'console_scripts': ['sail=sailboat.__main__:main', 'sailboat=sailboat.__main__:main'],
