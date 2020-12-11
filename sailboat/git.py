1283914cabee34bede4fbd2a08f5d6dbb7c38840
Cole Wilson <cole@colewilson.xyz>
2020-12-11 20:14:15 +0000

    :rocket: Initial Commit!

--- sailboat/git.py
+++ sailboat/git.py
@@ -16,6 +16,7 @@ def main():
 				user = input('What is your GitHub username? ')
 				al = user+"/"+data['short_name']
 			os.system(f'git init;git config --global credential.helper "cache --timeout=3600";git config user.name "{data["author"]}";git config user.email "{data["email"]}";git add .;git commit -m ":rocket: Initial Commit!";git remote add origin https://github.com/{al}.git;git push -u origin master;')
+			data['git']['github']=al
 		else:
 			data['git']['github']=''
 	open('.'+os.sep+'sailboat.toml','w+').write(toml.dumps(data))
