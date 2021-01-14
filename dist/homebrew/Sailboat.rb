c774d116d534ba466bceca8d51b4b56a044304f2
Cole Wilson <cole@colewilson.xyz>
2021-01-14 21:58:52 +0000

    none

--- dist/homebrew/Sailboat.rb
+++ dist/homebrew/Sailboat.rb
@@ -1,6 +1,6 @@
 # Created with sailboat, the Python releaser
 
-# v0.12.3-rc.2+0b0a10e.1
+# v0.12.3-rc5
 
 class Sailboat < Formula
   include Language::Python::Virtualenv
--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@ except:
 
 options = {
 	"name":"sailboat",
-	"version":"0.12.3-rc.2",
+	"version":"0.12.3-rc5",
 	"scripts":[],
 	"entry_points":{'console_scripts': ['sail=sailboat.__main__:main', 'sailboat=sailboat.__main__:main'], 'sailboat_plugins': ['pypi=sailboat.builders:PyPi', 'homebrew=sailboat.builders:Homebrew', 'echo=sailboat.__main__:echo', 'pyinstaller=sailboat.builders:PyInstaller', 'dev=sailboat.core:Develop', 'release=sailboat.core:Release', 'build=sailboat.build:Build', 'wizard=sailboat.core:Wizard', 'remove=sailboat.core:Remove', 'add=sailboat.core:Add', 'plugins=sailboat.core:ManagePlugins', 'test=sailboat.tests:Test', 'git=sailboat.core:Git', 'tasks=sailboat.other:Tasks', 'workflow=sailboat.core:Actions', 'github=sailboat.core:GitHub']},
 	"author":"Cole Wilson",
